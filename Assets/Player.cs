74eea2b164654be861ee7cb948afc2773a91b8c3
bianccc01 <giorgiobiancini@gmail.com>
2022-10-19 19:56:48 +0200

    Aggiunti commenti nello script player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,27 +5,27 @@
 
     public class Player : MonoBehaviour
     {
-        public float moveSpeed = 3f;
-        private Vector2 moveInput;
 
-        private bool canMove = true;
         private Rigidbody2D rb;
 
-        public int salute = 5;
+        /*Componenti per il movimento*/
+        public float moveSpeed = 3f; //Velocita di movimento
+        private Vector2 moveInput; //Direzione stabilita dall'input dell'utente
+        private bool canMove = true; //se colpito dal nemico il personaggio non potrà muoversi fino a che non tocca terra
+        
 
-        //Componenti per il salto
+        public int salute = 5; //Test per un eventuale barra della salute
+
+        /*Componenti per il salto*/
         private bool jump = false;
-
-        private bool contatto = false;
-    
-        private bool isGrounded = false;
+        private bool isGrounded = false; 
         public float jumpamount = 100; //Altezza del salto
         public float gravityScale = 10; //Gravità per il salto verso l'alto
         public float fallingGravityScale = 40; //Gravità per la discesa verso il basso
 
         private void Start()
         {
-            rb = GetComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>(); //inizializzo le componenti necessarie per rendere il player un corpo rigido
         }
 
         void FixedUpdate()
@@ -39,6 +39,7 @@
             PlayerJump();
         }
 
+        /*Funzione che viene richiamata appena entra in collisione con un oggetto*/
         private void OnCollisionEnter2D(Collision2D theCollision)
         {
             if (theCollision.gameObject.tag == "Floor")
@@ -54,6 +55,8 @@
             
         }
 
+
+        /*Funzione che viene richiamata appena esce dalla collisione con un oggetto*/
         private void OnCollisionExit2D(Collision2D theCollision)
         {
             if (theCollision.gameObject.tag == "Floor")
@@ -76,8 +79,6 @@
             moveInput = value.Get<Vector2>();
         }
 
-
-
         void OnFire()
         {
             print("fired ");
@@ -94,13 +95,10 @@
 
         private void PlayerJump()
         {
-    
             if (jump == true && isGrounded == true)
             {
-                Debug.Log("Here");
                 rb.AddForce(Vector2.up * jumpamount, ForceMode2D.Impulse);
-
-
+                
                 if (rb.velocity.y >= 0)
                 {
                     rb.gravityScale = gravityScale;
