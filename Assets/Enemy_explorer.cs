7d136c235695ebb5a9687ac312c39feed20fa66f
Lorenzo Battisti <114929610+LorenzoBattistiRomaTre@users.noreply.github.com>
2022-10-28 10:20:17 +0200

    varie cose inutili

--- Assets/Enemy_explorer.cs
+++ Assets/Enemy_explorer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy_explorer : MonoBehaviour
 {
     public Transform player;
     public float moltiplicatoreDifficolta = 1f; //Temporaneo
