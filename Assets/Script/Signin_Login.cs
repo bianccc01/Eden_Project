c2a4d809d023ca42eb65eb6560a08a7e2b285406
alessandradb <ale.delbuono1@gmail.com>
2022-11-10 22:19:22 +0100

    Aggiunto script per registrazione e login

---
+++ Assets/Script/Signin_Login.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.SceneManagement;
+using System;
+using TMPro;
+using UnityEditor.PackageManager;
+using Unity.VisualScripting;
+
+public class Signin_Login : MonoBehaviour
+{
+
+    [Header("UI")]
+    public TMP_Text messaggio;
+    public TMP_InputField emailR;
+    public TMP_InputField passwordR;
+    public TMP_InputField usernameR;
+
+    public void Button_Registrazione()
+    {
+        if (passwordR.text.Length < 6)
+        {
+            messaggio.text = "Password troppo corta! Inserisci " +
+                "almeno 6 caratteri.";
+            return;
+        }
+
+        if (usernameR.text.Length < 3 || usernameR.text.Length > 20)
+        {
+            messaggio.text = "Username non idoneo! Inserisci " +
+                "tra i 3 ed i 20 caratteri alfabetici e numerici.";
+            return;
+        }
+
+        var richiesta = new RegisterPlayFabUserRequest()
+        {
+            Email = emailR.text,
+            Password = passwordR.text,
+            Username = usernameR.text
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(richiesta, OnRegisterSuccess, OnErrorRegister);
+    }
+
+    void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        messaggio.text = "Ora sei registrato! Fai il login per iniziare!";
+    }
+
+    void OnErrorRegister(PlayFabError error)
+    {
+        messaggio.text = "Account non creato!";
+        Debug.Log($"Account non creato!\n {error.GenerateErrorReport()}");
+    }
+
+    public void Button_Login()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailR.text,
+            Password = passwordR.text
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnErrorLogin);
+
+
+    }
+
+    void OnLoginSuccess(LoginResult result)
+    {
+        messaggio.text = "Premi START per iniziare!";
+        Debug.Log($"Log in");
+    }
+
+    void OnErrorLogin(PlayFabError error)
+    {
+        messaggio.text = "Email o password non corretti";
+        Debug.Log($"Email o password non corretti!\n {error.GenerateErrorReport()}");
+    }
+
+
+
+    public void Button_Reset()
+    {
+
+    }
+
+
+}
