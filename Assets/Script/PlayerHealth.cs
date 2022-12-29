21ec579d1ea18f88d583618818920ca301d37d3d
alessandradb <ale.delbuono1@gmail.com>
2022-12-29 20:57:11 +0100

    Prove

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -67,6 +67,7 @@ public class PlayerHealth : MonoBehaviour
             }
             
             lifebar.SetMaxHealth(maxHealth);
+
         }
         
     }
